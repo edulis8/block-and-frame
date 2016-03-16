66b1884b31fd3d87bce3237a27959e5c0fe032ee
Eric Broberg <eric.edulis@gmail.com>
2016-03-15 21:51:37 -0700

    basic responsive navbar

--- app/containers/UserProfile.js
+++ app/containers/UserProfile.js
@@ -1,5 +1,46 @@
 import React from 'react';
 import helpers from '../utils/helpers';
+import { Link } from 'react-router';
+
+
+function MenuBar (props) {
+  return (
+    <div className="ui attached stackable menu">
+  <div className="ui container">
+  <Link 
+    className="item"
+    to={'/app'} >
+      <i className="home icon"></i>
+      Home
+    </Link>
+    <a className="item">
+      <i className="mail icon"></i> Messages
+    </a>
+  
+    <a className="item">
+      <i className="grid layout icon"></i> Host Something Awesome 
+    </a>
+    
+    <div className="ui simple dropdown item right item">
+      My Account
+      <i className="dropdown icon"></i>
+      <div className="menu">
+        <Link 
+          className="item"
+          to={'/profile'} >
+            <i className="edit icon"></i>
+            Edit Profile
+        </Link>
+        <a className="item"><i className="globe icon"></i> Choose Language</a>
+        <a className="item"><i className="settings icon"></i> Account Settings</a>
+      </div>
+    </div>
+    
+  </div>
+</div>
+  );
+}
+
 
 class UserProfile extends React.Component {
   constructor(props) {
@@ -31,29 +72,32 @@ class UserProfile extends React.Component {
   render() {
     return (
       <div>
-        <div>
-          User Profile:
-        </div>
-        <div>
-          Your email: { this.state.email }
-        </div>
-        { this.state.username ?
-          <p>{ this.state.username }</p> :
-          <p>You haven't set a display name</p> }
-        <div>
-        { this.state.city ? 
-          <p>{ this.state.city }</p> : 
-          <p>You haven't set a city</p> }
-        </div>
-        <div>
-        { this.state.country ? 
-          <p>{ this.state.country }</p> : 
-          <p>You haven't set a country</p> }
-        </div>
-        <div>
-        { this.state.bio ? 
-          <p>{ this.state.bio }</p> : 
-          <p>You haven't set a bio</p> }
+        <MenuBar />
+        <div className="ui raised very padded text container segment">
+          <div>
+            User Profile:
+          </div>
+          <div>
+            Your email: { this.state.email }
+          </div>
+          { this.state.username ?
+            <p>{ this.state.username }</p> :
+            <p>You haven't set a display name</p> }
+          <div>
+          { this.state.city ? 
+            <p>{ this.state.city }</p> : 
+            <p>You haven't set a city</p> }
+          </div>
+          <div>
+          { this.state.country ? 
+            <p>{ this.state.country }</p> : 
+            <p>You haven't set a country</p> }
+          </div>
+          <div>
+          { this.state.bio ? 
+            <p>{ this.state.bio }</p> : 
+            <p>You haven't set a bio</p> }
+          </div>
         </div>
       </div>
     );
