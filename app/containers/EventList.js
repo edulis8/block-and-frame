aeaa39fc4fbe5cc611340c45401aff2ae3fe540e
Brian Moore <bmoorebrian53@gmail.com>
2016-03-16 13:28:24 -0400

    small changes to eventlist

--- app/containers/EventList.js
+++ app/containers/EventList.js
@@ -1,14 +1,33 @@
 import React from 'react';
+import axios from 'axios';
 import Event from './Event';
 
 class EventList extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      data: '',
+    };
+  }
+
   componentDidMount() {
-    console.log('EventLists children', this.props);
+    axios.get('/api/events/')
+      .then((response) => {
+        console.log(response);
+        this.state.data = response.data;
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+    console.log(this.state.data);
   }
 
   render() {
     return (
-      <Event>.</Event>
+      <div className="eventList">
+        test
+      </div>
     );
   }
 }
