36f6baf9f08218f95cf726788b40116c844fc3db
Kris Burke <kris.burke611@gmail.com>
2016-03-24 12:59:47 -0400

    (refactor) Format quick edit on single event view

--- app/components/events/UniqueEventEdit.js
+++ app/components/events/UniqueEventEdit.js
@@ -2,42 +2,54 @@ import React from 'react';
 
 const UniqueEventEdit = (props) => {
   return (
-    <div className="ui massive relaxed list">
-      <div className="item">
-        <h1>You are now making your spread even more awesome!</h1>
-        <form className="eventEdit">
+    <div className="container">
+      <div className="ui small form">
+        <div className="two fields">   
+        
+          <div className="field">
+            <label>Spread: </label>
+            <div className="ui left input">
+              <input
+                type="text"
+                value={props.eventName}
+                placeholder={props.eventName}
+                onChange={props.editState}
+              />
+            </div>
+          </div>
+              
+          <div className="field">
+            <label>Location: </label>
+            <div className="ui left input">
+              <input
+                type="text"
+                value={props.location}
+                placeholder={props.location}
+                onChange={props.editState}
+              />
+            </div>
+          </div>
+        </div>
 
-          <input
-            className="eventName"
-            value={props.eventName}
-            placeholder={props.eventName}
-            onChange={props.editState}
-          />
-
-          <input
-            className="location"
-            value={props.location}
-            placeholder={props.location}
-            onChange={props.editState}
-          />
-
-          <input
-            className="description"
-            value={props.description}
-            placeholder={props.description}
-            onChange={props.editState}
-          /><br />
-
-          <button
-            className="save-changes ui tiny button"
-            onClick={props.setEdit}
-          >
-            <i className="icon save"></i>
-            Save changes
-          </button>
-
-        </form>
-      </div>
+        <div className="field">
+          <label>Description: </label>
+          <div className="ui left input">
+            <textarea
+              rows="3"
+              value={props.description}
+              placeholder={props.description}
+              onChange={props.editState}
+            />
+          </div>
+        </div>
+            
+        <div
+          className="ui tiny submit button"
+          onClick={props.setEdit}
+        >Save Changes
+        </div>
+      
+      </div>  
     </div>
   );
 };
