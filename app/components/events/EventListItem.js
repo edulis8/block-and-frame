e5ef2b33d9f7c2792ac070971d3b8088e9179bc0
Brian Moore <bmoorebrian53@gmail.com>
2016-03-30 17:30:01 -0400

    map in for feature freeze

--- app/components/events/EventListItem.js
+++ app/components/events/EventListItem.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router';
 import moment from 'moment';
 import instaHelpers from '../../utils/instaHelpers';
+import UniqueMapView from '../components/events/UniqueMapView';
 
 class Event extends React.Component {
   constructor(props) {
@@ -37,45 +38,45 @@ class Event extends React.Component {
     .add(1, 'day'); // not sure why a day has to be added
     
     return (
-          <div className="ui green centered card">
-            <div className="content">
-              <div className="right floated meta">
-                <div>{dateTime.format('MMMM Do YYYY, h:mm:ss a')}</div>
-              </div>
-                <div className="image host">
-                  <img className="ui avatar image" src={this.props.creatorInstaPic} />
-                  <div>Host: {this.props.creatorName} </div>
-                  <i className="tiny instagram icon"></i> @{this.props.creatorInstaname}
-                </div> 
-            </div>
-
-            <div className="image event-image">
-              <img className="eventlist-image" src={this.state.tagArray[this.state.random] || 'http://placehold.it/50x50'} />
-            </div>
-
-            <div className="content">
-              <Link to={`/${this.props.id}`} className="ui header">{this.props.name}</Link>
-              <div className="location">
-                {/* make sure the location and description get cut off if really long */}
-                <h5 className="ui header">
-                  {this.props.location.slice(0, 30)} {this.props.location.length > 30 && '...'}
-                </h5>
-
-              </div>
-              <div className="description">
-                {/* this.props.description.slice(0, 30)} {this.props.description.length > 30 && '...' */}
-                
-              </div>
-              <div className="extra content">
-                <span className="right floated">
-                  <i className="users outline icon"></i>
-                  {this.props.numAttendees} signed up
-                 </span>
-               <i className="comment icon"></i>
-                <label>3 comments</label>
-              </div>
-            </div>  
+      <div className="ui green centered card">
+        <div className="content">
+          <div className="right floated meta">
+            <div>{dateTime.format('MMMM Do YYYY, h:mm:ss a')}</div>
           </div>
+            <div className="image host">
+              <img className="ui avatar image" src={this.props.creatorInstaPic} />
+              <div>Host: {this.props.creatorName} </div>
+              <i className="tiny instagram icon"></i> @{this.props.creatorInstaname}
+            </div> 
+        </div>
+
+        <div className="image event-image">
+          <img className="eventlist-image" src={this.state.tagArray[this.state.random] || 'http://placehold.it/50x50'} />
+        </div>
+
+        <div className="content">
+          <Link to={`/${this.props.id}`} className="ui header">{this.props.name}</Link>
+          <div className="location">
+            {/* make sure the location and description get cut off if really long */}
+            <h5 className="ui header">
+              {this.props.location.slice(0, 30)} {this.props.location.length > 30 && '...'}
+            </h5>
+
+          </div>
+          <div className="description">
+            {/* this.props.description.slice(0, 30)} {this.props.description.length > 30 && '...' */}
+            
+          </div>
+          <div className="extra content">
+            <span className="right floated">
+              <i className="users outline icon"></i>
+              {this.props.numAttendees} signed up
+             </span>
+           <i className="comment icon"></i>
+            <label>3 comments</label>
+          </div>
+        </div>  
+      </div>
     );
   }
 }
